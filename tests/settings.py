4694b04e100bb6db4361d8c04a689f9eea07adf8
veoco <one@nomox.cn>
2022-09-04 13:44:37 +0800

    add more INSTALLED_APPS and MIDDLEWARE in tests

--- tests/settings.py
+++ tests/settings.py
@@ -6,8 +6,23 @@ BASE_DIR = Path(__file__).resolve().parent
 SECRET_KEY = 'fake-key'
 
 INSTALLED_APPS = [
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
     "tests",
     "speedtests",
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'speedtests.middleware.token_auth_middleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
 ROOT_URLCONF = 'tests.urls'
