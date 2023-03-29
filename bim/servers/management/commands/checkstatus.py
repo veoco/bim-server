5371b0dec2e538c16e48882f1b9c371e96c79e9f
veoco <one@nomox.cn>
2023-03-29 17:23:16 +0800

    新增定期检查命令

---
+++ bim/servers/management/commands/checkstatus.py
@@ -0,0 +1,18 @@
+from datetime import timedelta
+
+from django.core.management.base import BaseCommand
+from django.utils import timezone
+
+from servers.models import Machine, Task
+
+
+class Command(BaseCommand):
+    help = "Check Machine and Task status"
+
+    def handle(self, *args, **options):
+        due_time = timezone.now() - timedelta(minutes=1)
+
+        Machine.objects.filter(modified__lte=due_time).update(status=0)
+        Task.objects.filter(modified__lte=due_time, status=2).update(status=4)
+
+        self.stdout.write(self.style.SUCCESS("Successfully checked"))
